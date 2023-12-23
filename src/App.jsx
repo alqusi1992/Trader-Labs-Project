ff72cf19d9853b87475cfcd20757191c26febff7
alqusi1992 <malqusi@yahoo.com>
2023-12-23 23:46:04 +0100

    Added the Risk image

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import MainPage from './components/MainPage/MainPage.jsx';
 import Vision from './components/VisionPage/Vision.jsx';
 import LastWeek from './components/LastWeek/LastWeek.jsx';
 import ImagesSlide from './components/ImagesSlide/ImagesSlide.jsx';
+import Risk from './components/Risk/Risk.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Vision />
       <LastWeek />
       <ImagesSlide />
+      <Risk />
     </div>
   );
 }
---
+++ src/components/Risk/Risk.jsx
@@ -0,0 +1,15 @@
+import RiskImg from '../../assets/Risk.png';
+import './Risk.css';
+
+function Risk() {
+  return (
+    <div>
+      {' '}
+      <section className='Risk'>
+        <img src={RiskImg} alt='Trader Labs' />
+      </section>
+    </div>
+  );
+}
+
+export default Risk;
