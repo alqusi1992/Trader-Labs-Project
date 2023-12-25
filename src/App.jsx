03ac186e37d3da3f750ff7d989445979691454b8
alqusi1992 <malqusi@yahoo.com>
2023-12-25 05:24:17 +0100

    added scroll up arrow to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import './App.css';
 import MainPage from './components/MainPage/MainPage.jsx';
 import Vision from './components/VisionPage/Vision.jsx';
@@ -6,8 +8,38 @@ import ImagesSlide from './components/ImagesSlide/ImagesSlide.jsx';
 import Risk from './components/Risk/Risk.jsx';
 
 function App() {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const checkScroll = () => {
+    if (!showScrollButton && window.pageYOffset > 150) {
+      setShowScrollButton(true);
+    } else if (showScrollButton && window.pageYOffset <= 100) {
+      setShowScrollButton(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', checkScroll);
+
+    return () => {
+      window.removeEventListener('scroll', checkScroll);
+    };
+  }, [showScrollButton]);
+
   return (
     <div className='container-app'>
+      {showScrollButton && (
+        <button id='scrollToTopBtn' onClick={scrollToTop}>
+          ↑
+        </button>
+      )}
       <MainPage />
       <Vision />
       <LastWeek />
